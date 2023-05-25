8a9cb6b522c66a70a7187a673bf9851983fe003a
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2023-05-25 12:44:31 +0200

    Saving everything

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-gradient-to-br  from-slate-900 to-zinc-950 min-h-screen">
+      <div className="container mx-auto max-w-4xl min-h-screen">
+        <video className="rounded border border-slate-500 min-h-[50%]" controls>
+          <source src="movie.mp4" type="video/mp4" />
+          <source src="movie.ogg" type="video/ogg" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </div>
   );
 }
